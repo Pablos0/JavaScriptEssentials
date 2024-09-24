523cc83fd18b1121c93548be983ae4e30650f12a
Pablos <pablo.salcedom@gmail.com>
2024-09-24 12:54:44 -0400

    practicing array methods

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,6 +1,6 @@
 const employees = [
-    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    {id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
+    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+    {id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Java' },
     {id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
     // ... More employee records can be added here
 ];
@@ -29,4 +29,10 @@ function findEmployeeById(employeeId) {
     } else {
         document.getElementById('employeesDetails').innerHTML = 'No employee has been found with this ID';
     }
+}
+
+function findBySpecialization() {
+    const employeeSpecialization = employees.filter(employee => employee.specialization === 'JavaScript');
+        const specializationDisplay = employeeSpecialization.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.age} - ${employee.department} - ${employee.specialization}</p>`).join('');
+        document.getElementById('employeesDetails').innerHTML = specializationDisplay;
 }
